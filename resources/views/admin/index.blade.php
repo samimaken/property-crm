fbf67338c97b5d439e06449c7358ac6b78928886
Sami <sami@pop-os.localdomain>
2023-02-10 14:46:34 +0500

    changes

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -10,48 +10,18 @@
 @endsection
 
 @section('pagelevel_stylesheets')
-    
+
 @endsection
 
 @section('content')
   <div class="page-content">
-    <!-- BEGIN PAGE HEADER-->
-    <!-- BEGIN PAGE BAR -->
-    <div class="page-bar">
-        <ul class="page-breadcrumb">
-            <li>
-                <a href="index.html">Home</a>
-                <i class="fa fa-circle"></i>
-            </li>
-            <li>
-                <span>Dashboard</span>
-            </li>
-        </ul>
-        <div class="page-toolbar">
-            <div id="dashboard-report-range" class="pull-right tooltips btn btn-sm" data-container="body" data-placement="bottom" data-original-title="Change dashboard date range">
-                <i class="icon-calendar"></i>&nbsp;
-                <span class="thin uppercase hidden-xs"></span>&nbsp;
-                <i class="fa fa-angle-down"></i>
-            </div>
-        </div>
-    </div>
-    <!-- END PAGE BAR -->
-    <!-- BEGIN PAGE TITLE-->
-    <h1 class="page-title"> Admin Dashboard
-        <small>statistics, charts, recent events and reports</small>
-    </h1>
-    <!-- END PAGE TITLE-->
 
-    <!-- Start Page Content-->
-    
-    <!-- End Page Content-->
-    
-</div>
+   </div>
 
 @endsection
 
 @section('pagelevel_plugins')
-   
+
 <script src="{{ asset('admin-assets/global/plugins/moment.min.js')}}" type="text/javascript"></script>
 <script src="{{ asset('admin-assets/global/plugins/bootstrap-daterangepicker/daterangepicker.min.js')}}" type="text/javascript"></script>
 <script src="{{ asset('admin-assets/global/plugins/morris/morris.min.js')}}" type="text/javascript"></script>
